d3318081ce46c5199f160f3c4c661dfb98164e83
kellycesario <kellycesario@gmail.com>
2022-10-05 00:03:46 -0300

    Começando a criar e a estilizar a homescreen

---
+++ AutoApp/src/Screens/Home/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonArea: {
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+    margin: 15,
+  },
+  button: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#182E3A",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 0,
+    
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    marginTop: 30,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
