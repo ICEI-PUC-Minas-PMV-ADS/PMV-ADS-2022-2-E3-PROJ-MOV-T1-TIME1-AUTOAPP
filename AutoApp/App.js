ce4b5e6c2857381741ded5d49bff7ad589760686
Pedro Daniel Jardim <pedrodanieljardim@gmail.com>
2022-10-06 00:08:58 -0300

    feat: tela de cadastro de motoristas

--- AutoApp/App.js
+++ AutoApp/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import Cadastro from './src/Components/CadastroProp/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cadastro />
       <StatusBar style="auto" />
     </View>
   );
