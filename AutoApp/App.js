6385c9905901cefac99e1363fd0c273addbec8f7
kellycesario <kellycesario@gmail.com>
2022-10-06 21:53:29 -0300

    Add instrução de visualização das telas feitas

--- AutoApp/App.js
+++ AutoApp/App.js
@@ -1,27 +1,15 @@
-import { StatusBar, StyleSheet, View } from 'react-native';
-import Login from '../AutoApp/src/Screens/Login'
-import ChooseProfile from '../AutoApp/src/Screens/ChooseProfile'
-import OwnerRegistration from '../AutoApp/src/Screens/OwnerRegistration'
-import GarageRegistration from '../AutoApp/src/Screens/GarageRegistration';
-import Home from '../AutoApp/src/Screens/Home'
+import { View } from "react-native";
+import Login from "../AutoApp/src/Screens/Login";
+import ChooseProfile from "../AutoApp/src/Screens/ChooseProfile";
+import OwnerRegistration from "../AutoApp/src/Screens/OwnerRegistration";
+import GarageRegistration from "../AutoApp/src/Screens/GarageRegistration";
+import Home from "../AutoApp/src/Screens/Home";
 
 export default function App() {
-  
   return (
-    <View style={styles.container}>
- 
-      <Home />
-
-      <StatusBar style="auto" />
+    <View>
+      {/*Para visualizar as telas desenvolvidas, basta trocar o nome a seguir. Ex: <Login />, <Home />, etc. e dar npm start para abrir o QR code do Expo Go*/}
+      <Login />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
