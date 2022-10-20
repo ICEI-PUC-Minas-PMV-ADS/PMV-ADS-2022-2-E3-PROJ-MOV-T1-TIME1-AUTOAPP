310623d18902f092f85d5e1d3f9baf15a3b2507e
kellycesario <kellycesario@gmail.com>
2022-10-19 23:13:30 -0300

    Desenvolvimento das telas 8-13-16-20-23

--- AutoApp/App.js
+++ AutoApp/App.js
@@ -5,13 +5,17 @@ import OwnerRegistration from "../AutoApp/src/Screens/OwnerRegistration";
 import GarageRegistration from "../AutoApp/src/Screens/GarageRegistration";
 import Home from "../AutoApp/src/Screens/Home";
 
+import DataAlteration from '../AutoApp/src/Screens/ChangeData'
 import MyAutos from '../AutoApp/src/Screens/MyAutomobiles'
+import Services from '../AutoApp/src/Screens/RegisterService'
+import Ads from '../AutoApp/src/Screens/RegisterAd'
+import MyMaintenancesDone from '../AutoApp/src/Screens/MyMaintenances'
 
 export default function App() {
   return (
     <View>
       {/*Para visualizar as telas desenvolvidas, basta trocar o nome a seguir. Ex: <Login />, <Home />, etc. e rodar o comando npm start para abrir o QR code do Expo Go*/}
-      <MyAutos />
+      <DataAlteration />
     </View>
   );
 }
---
+++ AutoApp/src/Screens/MyMaintenances/index.js
@@ -0,0 +1,68 @@
+import { View, ScrollView } from "react-native";
+import { useState } from "react"
+import { TextInput } from "react-native-paper";
+import DefaultButton from "../../Components/Buttons/Default";
+import CancelButton from "../../Components/Buttons/Cancel";
+import Nav from "../../Components/NavBar";
+import Statusbar from "../../Components/StatusBar";
+import { styles } from "./styles";
+
+const form = {
+  oilChange: "",
+  engineOverhaul: "",
+  airConditioning: "",
+};
+
+const MyMaintenancesDone = () => {
+  const [oilChange, setOilChange] = useState("")
+  const [engineOverhaul, setEngineOverhaul] = useState("")
+  const [airConditioning, setAirConditioning] = useState("")
+
+
+  return (
+    <ScrollView>
+      <Statusbar />
+      <Nav />
+      <View style={styles.container}>
+        <View>
+        <TextInput
+          style={styles.input}
+          label="Troca de óleo"
+          placeholder=""
+          onChangeText={(oilChange) => setOilChange(oilChange)}
+          mode="outlined"
+          activeOutlineColor="#182E3A"
+          outlineColor="#182E3A"
+          right={<TextInput.Icon icon="square-edit-outline" />}  
+        />
+        </View>
+
+        <TextInput
+          style={styles.input}
+          label="Revisão de motor"
+          placeholder=""
+          onChangeText={(engineOverhaul) => setEngineOverhaul(engineOverhaul)}
+          mode="outlined"
+          activeOutlineColor="#182E3A"
+          outlineColor="#182E3A"
+          right={<TextInput.Icon icon="square-edit-outline" />}  
+        />
+
+        <TextInput
+          style={styles.input}
+          label="Revisão de ar condicionado"
+          placeholder=""
+          onChangeText={(airConditioning) => setAirConditioning(airConditioning)}
+          mode="outlined"
+          activeOutlineColor="#182E3A"
+          outlineColor="#182E3A"
+          right={<TextInput.Icon icon="square-edit-outline" />}  
+        />
+
+        <DefaultButton text={"Salvar"} />
+        <CancelButton text={"Cancelar"} />
+      </View>
+    </ScrollView>
+  );
+};
+export default MyMaintenancesDone;
