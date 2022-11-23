fa66e7b4256a114b01c83f2a0551e05876943cef
Renatadiniz <90404960+Renatadiniz@users.noreply.github.com>
2022-11-23 14:55:04 -0300

    integração cadastrar anuncios
    @pedrodanieljardim e @kellycesario favor validarem

--- AutoApp/src/Screens/23-RegisterAd/index.js
+++ AutoApp/src/Screens/23-RegisterAd/index.js
@@ -1,36 +1,77 @@
 import { View, ScrollView } from "react-native";
-import { useNavigation } from '@react-navigation/native'
-import { useState } from "react"
+import { useNavigation, Alert } from '@react-navigation/native'
+import { useState, useEffect } from "react"
 import { TextInput } from "react-native-paper";
 import DefaultButton from "../../Components/Buttons/Default";
 import CancelButton from "../../Components/Buttons/Cancel";
 import Nav from "../../Components/NavBar";
 import Statusbar from "../../Components/StatusBar";
 import { styles } from "./styles";
+import { DatabaseConnection } from '../../Database/connection';
+const db = DatabaseConnection.getConnection();
 
 const form = {
-  firstAd: "",
-  secondAd: "",
-  thirdAd: "",
-};
+  adName: "",
+  adDescription: "",
+  };
 
-const RegisterAd = () => {
+const AddAd = () => {
   const navigation = useNavigation();
-  const [firstAd, seFirstAd] = useState("")
-  const [secondAd, setSecondAd] = useState("")
-  const [thirdAd, setThirdAd] = useState("")
+  const [adName, setadName] = useState("")
+  const [adDescription, setadDescription] = useState("")
+  
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='ad_auto_app'",
+        [],
+        function (tx, res) {
+          console.log("item (useEffect):", res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql("DROP TABLE IF EXISTS ad_auto_app", []);
+            txn.executeSql(
+              "CREATE TABLE IF NOT EXISTS ad_auto_app(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), description VARCHAR(255))",
+              []
+            );
+          }
+        }
+      );
+    });
+  }, []);
+
+let registerAd = () => {
+    console.log("registerAd")
+    console.log(adName, adDescription);
+    
+    db.transaction(function (tx) {
+      tx.executeSql(
+        'INSERT INTO service_auto_app ( name , description) VALUES (?,?)',
+        [adName, adDescription],
+        (tx, results) => {
+          console.log('Results', results.rowsAffected);
+          console.log(results)
+
+          if (results.rowsAffected > 0) {
+
+            alert("Anúncio Cadastrado com Sucesso !!!");
+            navigation.navigate("MyAdds", {});
+          } else alert('Erro ao tentar Cadastrar o Anúncio !!!');
+        }
+      );
+    });
+  };
 
   return (
     <ScrollView>
       <Statusbar />
-      <Nav onPress={() => navigation.navigate("Garages")} />
+      <Nav onPress={() => navigation.navigate("MyAdds")} />
       <View style={styles.container}>
         <View>
+
         <TextInput
           style={styles.input}
-          label="Anúncio 1"
-          placeholder=""
-          onChangeText={(firstAd) => seFirstAd(firstAd)}
+          label="Anuncio"
+          onChangeText={(adName) => setadName (adName)}
           mode="outlined"
           activeOutlineColor="#182E3A"
           outlineColor="#182E3A"
@@ -40,30 +81,19 @@ const RegisterAd = () => {
 
         <TextInput
           style={styles.input}
-          label="Anúncio 2"
-          placeholder=""
-          onChangeText={(secondAd) => setSecondAd(secondAd)}
+          label="Descricao do Anuncio"
+          onChangeText={(adDescription) => setadDescription (adDescription)}
           mode="outlined"
           activeOutlineColor="#182E3A"
           outlineColor="#182E3A"
           right={<TextInput.Icon icon="square-edit-outline" />}  
         />
 
-        <TextInput
-          style={styles.input}
-          label="Anúncio 3"
-          placeholder=""
-          onChangeText={(thirdAd) => setThirdAd(thirdAd)}
-          mode="outlined"
-          activeOutlineColor="#182E3A"
-          outlineColor="#182E3A"
-          right={<TextInput.Icon icon="square-edit-outline" />}  
-        />
-
-        <DefaultButton text={"Salvar"} />
+         <DefaultButton text={"Salvar"} onPress={registerAd} />
         <CancelButton text={"Cancelar"} />
       </View>
     </ScrollView>
   );
 };
 export default RegisterAd;
+
