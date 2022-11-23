fb1733c32023c72ed50d424394a1bdbdae1174d9
Renatadiniz <90404960+Renatadiniz@users.noreply.github.com>
2022-11-23 12:40:37 -0300

    Integração Meus anúncios
    @pedrodanieljardim , favor validar

--- AutoApp/src/Screens/21-MyAdds/index.js
+++ AutoApp/src/Screens/21-MyAdds/index.js
@@ -1,36 +1,87 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, FlatList } from "react-native";
 import { useNavigation } from '@react-navigation/native'
 import Nav from "../../Components/NavBar/index";
 import DefaultButton from "../../Components/Buttons/Default";
 import List from "../../Components/List";
 import { styles } from "./styles";
+import { useState, useEffect } from "react";
+import { DatabaseConnection } from "../../Database/connection";
+const db = DatabaseConnection.getConnection();
+
+let adList = []
+
+
 
 const MyAdds = () => {
   const navigation = useNavigation();
+  let [flatListItems, setFlatListItems] = useState([]);
+  
+  
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='ad_auto_app'",
+        [],
+        function (tx, res) {
+          console.log("item (useEffect):", res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql("DROP TABLE IF EXISTS ad_auto_app", []);
+            txn.executeSql(
+              "CREATE TABLE IF NOT EXISTS car_auto_app(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), description VARCHAR(255))",
+              []
+            );
+          }
+        }
+      );
+    });
+
+    db.transaction(function (tx) {
+      tx.executeSql(
+        "SELECT * FROM ad_auto_app ",
+        [],
+        (tx, results) => {
+          let len = results.rows.length;
+          if (len > 0) {
+            alert("Anúncios carregados com sucesso!");
+            this.carList = results.rows;
+            console.log(this.adList._array)
+            setFlatListItems(this.adList._array);
+          } else {
+            alert("Anúncios não encontrados!");
+          }
+        }
+      );
+    });
+
+  }, []);
+
+
+  let listItemView = (item) => {
+
+    return (
+      <View>
+        <List 
+        text= {item.model}
+        onPress={() => navigation.navigate('ModalMyAds')}
+        />
+      </View>
+    );
+  };
+
+  
   return (
     <ScrollView>
       <Nav onPress={() => navigation.navigate("Garages")} />
       <View style={styles.container}>
-        <List
-          source={require("../../../assets/images/tools.png")}
-          text="Anúncio 1"
-          onPress={() => navigation.navigate('')}
+        
+      <FlatList
+            data={flatListItems}
+            renderItem={({ item }) => listItemView(item)}
         />
 
-        <List
-          source={require("../../../assets/images/tools.png")}
-          text="Anúncio 2"
-          onPress={() => navigation.navigate('')}
-        />
-
-        <List
-          source={require("../../../assets/images/tools.png")}
-          text="Anúncio 3"
-          onPress={() => navigation.navigate('')}
-        />
         <DefaultButton 
         text="Adicionar anúncio"
-        onPress={() => navigation.navigate('RegisterAd')}
+        onPress={() => navigation.push('RegisterAd')}
         />
       </View>
     </ScrollView>
