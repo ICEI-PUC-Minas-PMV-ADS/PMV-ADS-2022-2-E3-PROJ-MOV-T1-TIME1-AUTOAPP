eee44cad7cff312f0a469cc8febc006948ff16d0
Renatadiniz <90404960+Renatadiniz@users.noreply.github.com>
2022-11-23 14:43:35 -0300

    integração Minhas Manutenções
    @pedrodanieljardim e @kellycesario favor validarem

--- AutoApp/src/Screens/14-MyMaintenances/index.js
+++ AutoApp/src/Screens/14-MyMaintenances/index.js
@@ -1,31 +1,84 @@
-import { ScrollView, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { ScrollView, View, FlatList } from "react-native";
+import { useNavigation, Alert } from "@react-navigation/native";
 import Nav from "../../Components/NavBar/index";
 import DefaultButton from "../../Components/Buttons/Default";
 import List from "../../Components/List";
 import { styles } from "./styles";
+import { useState, useEffect } from "react";
+import { DatabaseConnection } from "../../Database/connection";
+const db = DatabaseConnection.getConnection();
+
+let mainList = []
 
 const MyMaintenances = () => {
-  const navigation = useNavigation();
+const navigation = useNavigation();
+  let [flatListItems, setFlatListItems] = useState([]);
+
+useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='main_auto_app'",
+        [],
+        function (tx, res) {
+          console.log("item (useEffect):", res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql("DROP TABLE IF EXISTS main_auto_app", []);
+            txn.executeSql(
+              "CREATE TABLE IF NOT EXISTS car_auto_app(id INTEGER PRIMARY KEY AUTOINCREMENT, mainName VARCHAR(255), mainDescription VARCHAR(255), mainDate DATE)",
+              []
+            );
+          }
+        }
+      );
+    });
+
+    db.transaction(function (tx) {
+      tx.executeSql(
+        "SELECT * FROM main_auto_app ",
+        [],
+        (tx, results) => {
+          let len = results.rows.length;
+          if (len > 0) {
+            alert("Manutenções carregadas com sucesso!");
+            this.mainList = results.rows;
+            console.log(this.mainList._array)
+            setFlatListItems(this.mainList._array);
+          } else {
+            alert("Manutenções não encontradas!");
+          }
+        }
+      );
+    });
+
+  }, []);
+
+
+  let listItemView = (item) => {
+
+    return (
+      <View>
+        <List 
+        text= {item.model}
+        onPress={() => navigation.navigate('ModalMaintenance')}
+        />
+      </View>
+    );
+  };
+
 
   return (
     <ScrollView>
       <Nav onPress={() => navigation.navigate("Owners")} />
       <View style={styles.container}>
-        <List
-          source={require("../../../assets/images/pulse.png")}
-          text="Troca de óleo"
-          onPress={() => navigation.navigate("")}
-        />
 
-        <List
-          source={require("../../../assets/images/car.png")}
-          text="Filtro de ar"
-          onPress={() => navigation.navigate("")}
-        />
+  <FlatList
+            data={flatListItems}
+            renderItem={({ item }) => listItemView(item)}
+/>
+
         <DefaultButton
           text="Adicionar manutenção"
-          onPress={() => navigation.navigate("AddMaintenance")}
+          onPress={() => navigation.push("AddMaintenance")}
         />
       </View>
     </ScrollView>
@@ -33,3 +86,4 @@ const MyMaintenances = () => {
 };
 
 export default MyMaintenances;
+
